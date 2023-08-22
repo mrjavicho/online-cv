5bddd6d0cef51af12b53829a523fe49205b95616
Javier Fernando Huamanchumo Arauco <javier.huamanchumo@gmail.com>
2023-08-22 20:28:32 +0200

    v1
    First CV

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,14 +8,34 @@ jQuery(document).ready(function($) {
         
             var itemWidth = $(this).data('level');
             
+            try {
+                var highColor = getComputedStyle(document.documentElement).getPropertyValue('--high-color').trim();
+                var mediumColor = getComputedStyle(document.documentElement).getPropertyValue('--medium-color').trim();
+                var lowColor = getComputedStyle(document.documentElement).getPropertyValue('--low-color').trim();
+            
+                console.log('highColor:', highColor);
+                console.log('mediumColor:', mediumColor);
+                console.log('lowColor:', lowColor);
+                console.log(highColor);
+                console.log(mediumColor);
+                var itemWidthNumber = parseInt(itemWidth.replace('%', ''), 0);
+                console.log(itemWidthNumber);
+                if (itemWidthNumber > 85) {
+                    console.log(this);
+                    this.style.backgroundColor =  highColor;
+                } else if (itemWidthNumber > 60) {
+                    this.style.backgroundColor =  mediumColor;
+                } else {
+                    this.style.backgroundColor =  lowColor;
+                }    
+            } catch (error) {
+                console.log(error)
+            }            
+            console.log(this) 
+
             $(this).animate({
                 width: itemWidth
-            }, 800);
-            
+            }, 800);            
         });
-
-    });
-   
-    
-
+    });       
 });
